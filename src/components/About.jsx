c6a2fcc7b81f20bb28ce44b1c10c89ee21fad79a
dk5488 <divypandey104@gmail.com>
2024-07-30 10:14:53 +0530

    contact me section added and education section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,25 +16,29 @@ function About() {
       </div>
 
       {/* Education */}
-      <div>
+      <div className='flex flex-col items-center justify-center gap-48 mt-16'>
         <h1>Education</h1>
 
-        <div>
+        <div className='flex flex-col'>
 
-          <div>
+          <div className=' h-24 border-l-2 border-l-white pl-10 flex flex-col  justify-center '>
             <div>B.Tech in Computer Science and Engineering</div>
             <div>SRM Institute of Scince and Technology</div>
             <div>2021-2025</div>
           </div>
 
+          <div className=' bg-pure-greys-300 w-[2px] h-24 '></div>
 
-          <div>
+
+          <div className=' h-24 border-l-2 border-l-pure-greys-300 pl-10 flex flex-col  justify-center '>
             <div>Class 12</div>
             <div>St. John's School</div>
             <div>2020-2021</div>
           </div>
 
-          <div>
+          <div className=' bg-pure-greys-300 w-[2px] h-24 '></div>
+
+          <div className=' h-24 border-l-2 border-l-pure-greys-300 flex pl-10 flex-col  justify-center'>
             <div>Class 10</div>
             <div>St. John's School</div>
             <div>2018-2019</div>
@@ -42,6 +46,12 @@ function About() {
 
 
         </div>
+      </div>
+
+      {/*Contact Me */}
+      <div className=' mt-9 flex flex-col justify-center items-center '>
+        <h1>Contact Me</h1>
+        <ContactUsForm/>
       </div>
     </div>
   )
