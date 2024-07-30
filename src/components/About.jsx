ded4c1de517d3b4f77c1e5b0ae3c767a878fa921
dk5488 <divypandey104@gmail.com>
2024-07-30 09:30:48 +0530

    Education section initialised

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
-
+import ContactUsForm from "../components/ContactUsForm";
+import dpppppp from "../assets/dpppppp.jpg";
+import dwnld from "../assets/dwnld.gif"
 function About() {
-  return <h1>About Page</h1>;
+  return (
+    <div>
+      {/*Hero Section */}
+      <div className='flex flex-row gap-8 items-center justify-center'>
+        <img src={dpppppp} alt='dp-1'/>
+        <div className=' flex flex-col gap-3'>
+           <h1>Divy Prakash Pandey</h1>
+           <h2>Full Stack Web Developer</h2>
+           <button className=' flex flex-row'>Resume<span><img src={dwnld} alt='download-icon'  className=" w-10 h-10"/></span></button>
+        </div>
+      </div>
+
+      {/* Education */}
+      <div>
+        <h1>Education</h1>
+
+        <div>
+
+          <div>
+            <div>B.Tech in Computer Science and Engineering</div>
+            <div>SRM Institute of Scince and Technology</div>
+            <div>2021-2025</div>
+          </div>
+
+
+          <div>
+            <div>Class 12</div>
+            <div>St. John's School</div>
+            <div>2020-2021</div>
+          </div>
+
+          <div>
+            <div>Class 10</div>
+            <div>St. John's School</div>
+            <div>2018-2019</div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default About;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,20 +4,19 @@ import ProjectPageGrid from "./projectPageGrid";
 
 function Projects() {
   return (
-    
     <div className=" overflow-x-hidden max-w-full">
       {/*Project Section */}
       <div>
         {/* Project Page title*/}
 
-        <div >
+        <div>
           <h1>Projects</h1>
           <span>Project List</span>
         </div>
 
         {/*Project Grid*/}
         <div className="w-5/6 mx-auto translate-x-14">
-          <div className="grid grid-cols-2 gap-16 items-center justify-center mx-auto translate-x-20" >
+          <div className="grid grid-cols-2 gap-16 items-center justify-center mx-auto translate-x-20">
             {ProjectPageData.map((index, key) => (
               <ProjectPageGrid
                 imager={index.image}
